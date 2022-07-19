dcb5b9fafb9bd48f9d948fc6a318e5785b06948e
anirudtate <anirud.programming@gmail.com>
2022-07-19 23:46:27 +0530

    font change

--- config.h
+++ config.h
@@ -4,17 +4,17 @@
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"monospace:size=10",
-	"NotoColor:pixelsize=8:antialias=true:autohint=true"
+	"JetBrains Mono:size=10",
+	"JoyPixels:size=10"
 };
-static const unsigned int bgalpha = 0xe0;
+static const unsigned int bgalpha = 0xe6;
 static const unsigned int fgalpha = OPAQUE;
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
-	[SchemeNorm] = { "#bbbbbb", "#222222" },
-	[SchemeSel] = { "#eeeeee", "#005577" },
-	[SchemeOut] = { "#000000", "#00ffff" },
+	[SchemeNorm] = { "#ebdbb2", "#000000" },
+	[SchemeSel] = { "#000000", "#ebdbb2" },
+	[SchemeOut] = { "#000000", "#ebdbb2" },
 };
 static const unsigned int alphas[SchemeLast][2] = {
 	/*		fgalpha		bgalphga	*/
